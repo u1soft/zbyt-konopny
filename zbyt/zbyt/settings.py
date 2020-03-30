9250a51d5a0464bb565fee9e23e21a5989b56b16
Marcin Ułanowicz <marcinulanowicz@gmail.com>
2020-03-30 20:50:27 +0200

    Add dockerization with docker-compose

--- zbyt/zbyt/settings.py
+++ zbyt/zbyt/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = 'b3fr9o1wnk=r)ti5=l0x_+@v)7gar-#m@iafk*r90y1w+hbv=='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['u1soft.pl',
+                 'www.u1soft.pl'
+                 '127.0.0.1']
 
 # Application definition
 
