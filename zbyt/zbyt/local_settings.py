e0daea9496ab925152aea7e202edbe24093e393c
Marcin Ułanowicz <marcinulanowicz@gmail.com>
2020-04-02 13:30:51 +0000

    Rm local_settings

--- zbyt/zbyt/local_settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
-DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
